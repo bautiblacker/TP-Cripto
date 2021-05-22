e78aef94ac033f1f25c44c755354c1e2d9ebef09
Gaston Lifschitz <glifschitz@itba.edu.ar>
2021-05-22 20:28:20 -0300

    Squared matrix fuction implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,12 @@ import models.Config;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Config config = new Config(args);
-        BMPImage bmpImage = new BMPImage(config.getSecretImage().getPath());
+        BMPImage bmpImage = new BMPImage(config.getSecretImage().getPath(), config.getK());
+        List<List<String>> squaredMatrix = bmpImage.getSquaredMatrix();
     }
 }
--- src/main/java/models/BMPImage.java
+++ src/main/java/models/BMPImage.java
@@ -8,6 +8,7 @@ import utils.FileUtils;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +16,21 @@ import java.util.List;
 public class BMPImage {
     private byte[] header;
     private byte[] body;
+    private int width, height;
     private List<List<String>> binaryImage = new ArrayList<>();
 
-    public BMPImage(String path) throws FileNotFoundException {
+    public BMPImage(String path, int k) throws FileNotFoundException {
         File imageFile = FileUtils.parseFile(path);
         try {
             BufferedImage image = JDeli.read(imageFile);
-
-            int L = image.getData().getHeight();
+            height = image.getData().getHeight();
+            width = image.getData().getWidth();
+            int size = image.getData().getDataBuffer().getSize();
+            if(height%2 != 0 || size%k != 0) throw new IOException("Invalid image or k.");
             int index = 0;
-            for(int i = 0;i < L; i++){
+            for(int i = 0;i < height; i++){
                 List<String> aux = new ArrayList<>();
-                for(int j = 0;j < L; j++) {
+                for(int j = 0;j < width; j++) {
                     aux.add(Binary.getBinary(image.getData().getDataBuffer().getElem(index++)));
                     //System.out.println(image.getData().getDataBuffer().getElem(i) + " - " + Binary.getBinary(image.getData().getDataBuffer().getElem(i)));
                 }
@@ -37,4 +41,45 @@ public class BMPImage {
         }
     }
 
+    public List<List<String>> getSquaredMatrix() {
+        List<List<String>> squaredMatrix = new ArrayList<>();
+        for(int i = 0;i < height; i += 2){
+            for(int j = 0;j < width; j += 2) {
+                List<String> aux = new ArrayList<>();
+                aux.add(binaryImage.get(i).get(j));
+                aux.add(binaryImage.get(i).get(j+1));
+                aux.add(binaryImage.get(i+1).get(j));
+                aux.add(binaryImage.get(i+1).get(j+1));
+                squaredMatrix.add(aux);
+            }
+
+        }
+        return squaredMatrix;
+    }
+
+    /*public static void main(String[] args){
+        List<List<String>> example = new ArrayList<>();
+        for(int i = 0;i<8;i++){
+            List<String> aux = new ArrayList<>();
+            for(int j = 0;j<6;j++){
+                aux.add(String.valueOf(i*6 + j + 1));
+            }
+            example.add(aux);
+        }
+
+        printImage(example);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        printImage(getSquaredMatrix());
+    }
+
+    private static void printImage(List<List<String>> image) {
+        for(int i = 0;i< image.size();i++){
+            for(int j = 0;j< image.get(i).size();j++){
+                System.out.print(image.get(i).get(j) + "\t");
+            }
+            System.out.println("\n");
+        }
+    }*/
 }
