18efee04a6a67e5a30c6136007f958f11affdc9b
Bensas <juanbensa@gmail.com>
2021-06-23 09:28:05 -0300

    Runs on Pampero

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 mkdir -p ./out
 echo "Compiling program..."
-javac -classpath "./lib/image4j-0.7.jar:./lib/jdeli-trial.jar:./lib/lombok-1.18.20.jar:./lib/javafx-3.1.7.jar" src/main/java/engine/*.java src/main/java/Main.java src/main/java/interfaces/*.java src/main/java/models/*.java src/main/java/utils/*.java -d ./out
+javac -classpath "./lib/image4j-0.7.jar:./lib/jdeli-trial.jar:./lib/lombok-1.18.20.jar:./lib/javafx-base.jar" src/main/java/engine/*.java src/main/java/Main.java src/main/java/interfaces/*.java src/main/java/models/*.java src/main/java/utils/*.java -d ./out
 echo "Done!"
